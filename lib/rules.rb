a271fa1a2b3cb7683bf0a2a05cfcc973fe457719
Chike1990 <chibuikeuzoechina@gmail.com>
2021-05-10 19:28:02 +0100

    Create class for the linters rules

---
+++ lib/rules.rb
@@ -0,0 +1,11 @@
+class Rules
+  attr_reader :this_line
+
+  def initialize(this_line, line_number, line_above = nil)
+    @line_above = line_above
+    @line_number = line_number + 1
+    @this_line = this_line
+    @error_messages = []
+  end
+
+end 
